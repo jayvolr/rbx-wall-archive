8d2977bdbdc978ed85740c72ff50a700a5d3fcb0
jayvolr <jonlov4@gmail.com>
2018-11-02 23:28:56 -0400

    prod api

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 let state = {
-  posts: null,
-  query: null,
-  umm: 'yep'
+  posts: [],
+  userQuery: null,
+  bodyQuery: null
 }
 
-Vue.component('search-bar', {
+function sanitizePostsResponse(response) {
+  response.data.forEach(post => {
+    if (!post.poster) {
+      post.poster = {
+        user: {
+          username: 'Banned User'
+        }
+      }
+    }
+  })
+}
+
+Vue.component('user-search', {
   data ()  {
     return state
   },
   methods: {
     keyup: _event => {
-      axios.get(`http://localhost:3000/85654/1?user=${this.app.query.toLowerCase()}`)
+      const apiUrl = this.app.bodyQuery ? 
+        `https://rbx-wall-api.herokuapp.com/85654/1?user=${this.app.userQuery.toLowerCase()}&body=${this.app.bodyQuery.toLowerCase()}`:
+        `https://rbx-wall-api.herokuapp.com/85654/1?user=${this.app.userQuery.toLowerCase()}`
+
+      axios.get(apiUrl)
         .then(response => {
-          console.log(response.data)
+          sanitizePostsResponse(response)
           this.app.posts = response.data
         })
         .catch(err => {
@@ -20,7 +36,30 @@ Vue.component('search-bar', {
         })
     }
   },
-  template: '<input type="text" v-model="query" @keyup="keyup" placeholder="Search for a user"/>'
+  template: '<input type="text" v-model="userQuery" @keyup="keyup" placeholder="Search for a user"/>'
+})
+
+Vue.component('body-search', {
+  data ()  {
+    return state
+  },
+  methods: {
+    keyup: _event => {
+      const apiUrl = this.app.userQuery ? 
+        `https://rbx-wall-api.herokuapp.com/85654/1?user=${this.app.userQuery.toLowerCase()}&body=${this.app.bodyQuery.toLowerCase()}`:
+        `https://rbx-wall-api.herokuapp.com/85654/1?body=${this.app.bodyQuery.toLowerCase()}`
+
+      axios.get(apiUrl)
+        .then(response => {
+          sanitizePostsResponse(response)
+          this.app.posts = response.data
+        })
+        .catch(err => {
+          throw new Error(err)
+        })
+    }
+  },
+  template: '<input type="text" v-model="bodyQuery" @keyup="keyup" placeholder="Search for a specific post"/>'
 })
 
 var app = new Vue({
@@ -29,8 +68,9 @@ var app = new Vue({
     return state
   },
   mounted () {
-    axios.get('http://localhost:3000/85654/1')
+    axios.get('https://rbx-wall-api.herokuapp.com/85654/1')
       .then(response => {
+        sanitizePostsResponse(response)
         this.posts = response.data
       })
       .catch(err => {
